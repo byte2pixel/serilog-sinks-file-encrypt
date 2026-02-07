08c699b85137b3fe35d2efc40cf0e7159e59e7e7
thch-cubido <161628082+thch-cubido@users.noreply.github.com>
2026-02-07 21:54:21 +0100

    feat: Switch to stream cypher (#38)
    * feat: Switch to AES-GCM stream cypher
    
    * ref: Don't create a new AesGcm instance per flush
    
    * fix: Previously remved assert re-added
    
    * fix: Undo test changes
    
    * fix: Make the linter happy
    
    * ref: Moved escaping
    fix: Dispose AesGcm
    
    * doc: Crypto lib description update

--- src/Serilog.Sinks.File.Encrypt/Models/HeaderSection.cs
+++ src/Serilog.Sinks.File.Encrypt/Models/HeaderSection.cs
@@ -3,4 +3,4 @@ namespace Serilog.Sinks.File.Encrypt.Models;
 /// <summary>
 /// Represents a parsed header section from the encrypted file
 /// </summary>
-internal record HeaderSection(byte[] EncryptedKey, byte[] EncryptedIv);
+internal record HeaderSection(byte[] TagLength, byte[] Nonce, byte[] SessionKey);
